9651ed493070f32af8ed24371dd773109ca160c9
DidierF <didier.fuentes@gmail.com>
2015-03-23 19:29:51 -0400

    Views added to layout and view objects added to activity.

--- app/src/main/java/example/com/trendingcode/MainActivity.java
+++ app/src/main/java/example/com/trendingcode/MainActivity.java
@@ -1,17 +1,29 @@
 package example.com.trendingcode;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    EditText searchBox;
+    Button searchBtn;
+    ListView list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        searchBox = (EditText) findViewById(R.id.searchBox);
+        searchBtn = (Button) findViewById(R.id.searchBtn);
+        list = (ListView) findViewById(R.id.resultsList);
+
     }
 
 
