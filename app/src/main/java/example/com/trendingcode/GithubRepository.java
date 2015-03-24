9d4e5b5124104c3893e47ef46e87a753bc2bd4d7
Francis Brito <fr.br94@gmail.com>
2015-03-24 10:48:46 -0400

    Format repository class code.

--- app/src/main/java/example/com/trendingcode/GithubRepository.java
+++ app/src/main/java/example/com/trendingcode/GithubRepository.java
@@ -1,20 +1,18 @@
 package example.com.trendingcode;
 
-public class GithubRepository
-{
+public class GithubRepository {
     private Integer id;
     private String name;
     private String full_name;
     private String description;
-	private Integer stargazers_count;
-	private Integer watchers_count;
-	private String language;
+    private Integer stargazers_count;
+    private Integer watchers_count;
+    private String language;
 
     /**
      * Construct.
      */
-    public GithubRepository()
-    {		
+    public GithubRepository() {
     }
 
     /**
@@ -23,148 +21,127 @@ public class GithubRepository
 
     /**
      * Gets the id.
-     * 
+     *
      * @return id
      */
-    public Integer getID()
-    {
+    public Integer getID() {
         return id;
     }
 
     /**
      * Sets the id.
-     * 
-     * @param id
      *
+     * @param id
      */
-    public void setID(Integer val)
-    {
+    public void setID(Integer val) {
         this.id = val;
     }
 
-	 /**
+    /**
      * Gets the name.
-     * 
+     *
      * @return name
      */
-    public String getName()
-    {
+    public String getName() {
         return name;
     }
 
     /**
      * Sets the name.
-     * 
-     * @param name
      *
+     * @param name
      */
-    public void setName(String val)
-    {
+    public void setName(String val) {
         this.name = val;
     }
-	
+
     /**
      * Gets the full_name.
-     * 
+     *
      * @return full_name
      */
-    public String getFullName()
-    {
+    public String getFullName() {
         return full_name;
     }
 
     /**
      * Sets the full_namename.
-     * 
-     * @param full_namename
      *
+     * @param full_namename
      */
-    public void setFullName(String val)
-    {
+    public void setFullName(String val) {
         this.full_name = val;
     }
 
     /**
      * Gets the description.
-     * 
+     *
      * @return description
      */
-    public String getDescription()
-    {
+    public String getDescription() {
         return description;
     }
 
     /**
      * Sets the description.
-     * 
-     * @param description
      *
+     * @param description
      */
-    public void setDescription(String val)
-    {
+    public void setDescription(String val) {
         this.description = val;
     }
 
     /**
      * Gets the stargazers_count.
-     * 
+     *
      * @return stargazers_count
      */
-    public Integer getStargazersCount()
-    {
+    public Integer getStargazersCount() {
         return stargazers_count;
     }
 
     /**
      * Sets the stargazers_count.
-     * 
-     * @param stargazers_count
      *
+     * @param stargazers_count
      */
-    public void setStargazersCount(Integer val)
-    {
+    public void setStargazersCount(Integer val) {
         this.stargazers_count = val;
     }
 
     /**
      * Gets the watchers_count.
-     * 
+     *
      * @return watchers_count
      */
-    public Integer getWatchersCount()
-    {
+    public Integer getWatchersCount() {
         return watchers_count;
     }
 
     /**
      * Sets the watchers_count.
-     * 
-     * @param watchers_count
      *
+     * @param watchers_count
      */
-    public void setWatchersCount(Integer val)
-    {
+    public void setWatchersCount(Integer val) {
         this.watchers_count = val;
     }
-	
-	 /**
+
+    /**
      * Gets the language.
-     * 
+     *
      * @return language
      */
-    public String getLanguage()
-    {
+    public String getLanguage() {
         return language;
     }
 
     /**
      * Sets the language.
-     * 
-     * @param language
      *
+     * @param language
      */
-    public void setLanguage(String val)
-    {
+    public void setLanguage(String val) {
         this.language = val;
     }
 }
