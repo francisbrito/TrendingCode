3b23d85ce50e161f82d65d0f7ab0c31d80693dc8
Francis Brito <fr.br94@gmail.com>
2015-03-25 01:53:10 -0400

    Add Github search result parsing logic.

--- app/src/main/java/example/com/trendingcode/GithubSearchResult.java
+++ app/src/main/java/example/com/trendingcode/GithubSearchResult.java
@@ -1,22 +1,51 @@
 package example.com.trendingcode;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.ArrayList;
 
 /**
  * Created by francis on 03/24/15.
  */
 public class GithubSearchResult {
-    private final int mTotalCount;
-    private final ArrayList<GithubRepository> mItems;
+    private int mTotalCount;
+    private ArrayList<GithubRepository> mItems;
 
     public GithubSearchResult() {
         this.mTotalCount = 0;
         this.mItems = new ArrayList<>();
     }
 
-    public static GithubSearchResult fromJSON(String jsonStr) {
+    public int getTotalCount() {
+        return this.mTotalCount;
+    }
+
+    public ArrayList<GithubRepository> getItems() {
+        return this.mItems;
+    }
+
+    public static GithubSearchResult fromJSON(String jsonStr) throws JSONException {
         // TODO-Mie087: JSON Parsing.
         //              Check out GithubRepository.fromJSON as a example.
-        return null;
+        GithubSearchResult result = new GithubSearchResult();
+
+        JSONObject json = new JSONObject(jsonStr);
+
+        Integer totalCount = json.getInt("total_count");
+        result.mTotalCount = totalCount;
+
+        JSONArray items = json.getJSONArray("items");
+
+        for (int i = 0; i < items.length(); i++) {
+            JSONObject item = items.getJSONObject(i);
+
+            GithubRepository repo = GithubRepository.fromJSON(item);
+
+            result.mItems.add(repo);
+        }
+
+        return result;
     }
 }
